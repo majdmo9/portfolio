bbb6e3a1cf1e369c78ae231cdcc5306f286fc1f6
Majd Mousa <majdmousa@Majds-MacBook-Pro-2.local>
2024-04-10 23:29:21 +0300

    Change zip code

--- components/Info.tsx
+++ components/Info.tsx
@@ -1,12 +1,10 @@
 import { INFO } from "@/utils/constants";
-import React from "react";
-import Img from "./general/Img";
 
 const Info = () => {
   return (
     <div className="flex flex-col justify-start lg:items-start items-center gap-2">
       {INFO.map(el => el)}
-      <p className="font-medium text-lg">NAZARETH, ISRAEL ZIP CODE 1695500</p>
+      <p className="font-medium text-lg">NAZARETH, ISRAEL ZIP CODE 1613101</p>
     </div>
   );
 };
